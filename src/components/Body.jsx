e1e57a8a93ea528101f301a69f36d28a0318174c
adarshadarsh6305-a11y <adarshadarsh6305@gmail.com>
2026-05-17 21:19:21 +0530

    navigate to login page if not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,43 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import {Outlet} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {useSelector} from "react-redux";
+import {Base_Url} from "../constants";
+import axios from "axios";
+import {useEffect} from "react";
+
+
+import {useNavigate} from "react-router-dom";
+import {addUser} from "../utils/userSlice";
 
 const Body = () =>{
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    const user=useSelector((store)=>store.user);
+
+    const fetchUser=async ()=>{
+    if(user) return;
+        try{
+          const res=await axios.get(Base_Url+"/profile/view",
+    {withCredentials:true},);
+    dispatch(addUser(res.data));
+    console.log(res.data);
+          }
+        
+        catch(err){
+            if(err.status===401){
+               navigate("/login");
+            }
+            console.log(err);
+        }
+
+
+    };
+    useEffect(()=>{
+        fetchUser();
+    },[]);
+    
     return(
      <div>
         <Navbar/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import { useSelector } from "react-redux";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
 
   const user = useSelector((store) => store.user);
 
-  console.log(user);
+  //console.log(user);
 
   return (
 
     <div className="navbar bg-base-300 shadow-sm">
 
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">
+        <Link to="/" className="btn btn-ghost text-xl">
           DevTinder
-        </a>
+        </Link>
       </div>
 
       {user && (
@@ -48,7 +49,7 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
 
-              <li><a>Profile</a></li>
+              <li><Link to="/profile" >Profile</Link></li>
 
               <li><a>Settings</a></li>
 
