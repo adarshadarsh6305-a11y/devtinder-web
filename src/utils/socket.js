5ff27826f5ed844e985c4e11a1d4d16fd34551aa
adarshadarsh6305-a11y <adarshadarsh6305@gmail.com>
2026-06-25 14:04:51 +0530

    updated socket code

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,10 @@ import {Base_Url} from "../constants";
 import io from "socket.io-client";
 
 export const createSocket=()=>{
+    if(window.location.hostname === "localhost"){
     return io(Base_Url);
+    }
+    else{
+        return io("/",{path:"/api/socket.io"})
+    }
 }
